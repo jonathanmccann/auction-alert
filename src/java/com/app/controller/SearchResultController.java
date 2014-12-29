6e7e8991f00f8f45a8ba20a0ce82487ef03bfc5d
Jonathan McCann <jonathan.mccann@liferay.com>
2014-12-29 15:34:46 -0800

    Add in GNU license copyright and @author

--- src/java/com/app/controller/SearchResultController.java
+++ src/java/com/app/controller/SearchResultController.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
 package com.app.controller;
 
 import com.app.dao.impl.SearchQueryDAOImpl;
@@ -17,6 +31,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Jonathan McCann
+ */
 @Controller
 public class SearchResultController {
 
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -1,3 +1,19 @@
+<%--
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+--%>
+
 <%@ page contentType="text/html; charset=UTF-8" language="java" pageEncoding="UTF-8" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
