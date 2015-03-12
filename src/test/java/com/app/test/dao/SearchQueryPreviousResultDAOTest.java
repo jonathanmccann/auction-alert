89207d0a58d1593de3375ec8e53106445cd931f0
Jonathan McCann <jonathan.mccann@liferay.com>
2015-03-12 14:49:02 -0700

    Add in test

---
+++ src/test/java/com/app/test/dao/SearchQueryPreviousResultDAOTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.test.dao;
+
+import com.app.dao.impl.SearchQueryPreviousResultDAOImpl;
+import com.app.exception.DatabaseConnectionException;
+import com.app.test.BaseDatabaseTestCase;
+
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Jonathan McCann
+ */
+public class SearchQueryPreviousResultDAOTest extends BaseDatabaseTestCase {
+
+	@Override
+	public void doSetUp() throws DatabaseConnectionException {
+		_searchQueryPreviousResultDAOImpl =
+			new SearchQueryPreviousResultDAOImpl();
+	}
+
+	@Test
+	public void testSearchQueryDAO()
+		throws DatabaseConnectionException, SQLException {
+
+		// Test add
+
+		_searchQueryPreviousResultDAOImpl.addSearchQueryPreviousResult(
+			1, "1234");
+		_searchQueryPreviousResultDAOImpl.addSearchQueryPreviousResult(
+			1, "2345");
+		_searchQueryPreviousResultDAOImpl.addSearchQueryPreviousResult(
+			1, "3456");
+
+		// Test get
+
+		List<String> searchQueryPreviousResults =
+			_searchQueryPreviousResultDAOImpl.getSearchQueryPreviousResults(1);
+
+		List<String> expectedSearchQueryPreviousResults = new ArrayList<>();
+
+		expectedSearchQueryPreviousResults.add("1234");
+		expectedSearchQueryPreviousResults.add("2345");
+		expectedSearchQueryPreviousResults.add("3456");
+
+		Assert.assertEquals(
+			expectedSearchQueryPreviousResults, searchQueryPreviousResults);
+
+		// Test delete oldest entry
+
+		_searchQueryPreviousResultDAOImpl.deleteSearchQueryPreviousResult(1);
+
+		searchQueryPreviousResults =
+			_searchQueryPreviousResultDAOImpl.getSearchQueryPreviousResults(1);
+
+		Assert.assertEquals(2, searchQueryPreviousResults.size());
+
+		expectedSearchQueryPreviousResults.remove(0);
+
+		Assert.assertEquals(
+			expectedSearchQueryPreviousResults, searchQueryPreviousResults);
+
+		// Test delete all entries
+
+		_searchQueryPreviousResultDAOImpl.deleteSearchQueryPreviousResults(1);
+
+		searchQueryPreviousResults =
+			_searchQueryPreviousResultDAOImpl.getSearchQueryPreviousResults(1);
+
+		Assert.assertEquals(0, searchQueryPreviousResults.size());
+	}
+
+	private static SearchQueryPreviousResultDAOImpl
+		_searchQueryPreviousResultDAOImpl;
+
+}
