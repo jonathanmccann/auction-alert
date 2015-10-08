4e2e4e51905ed38e910491e0e6d055add5e1630a
Jonathan McCann <jonathan.mccann@liferay.com>
2015-10-08 08:48:20 -0700

    Autowire the DAO and test to confirm they are not null

---
+++ src/test/java/com/app/test/spring/SpringTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2015-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.test.spring;
+
+import com.app.dao.impl.CategoryDAOImpl;
+import com.app.dao.impl.ReleaseDAOImpl;
+import com.app.dao.impl.SearchQueryDAOImpl;
+import com.app.dao.impl.SearchQueryPreviousResultDAOImpl;
+import com.app.dao.impl.SearchResultDAOImpl;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Jonathan McCann
+ */
+@ContextConfiguration("/test-dispatcher-servlet.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SpringTest {
+
+	@Autowired
+	CategoryDAOImpl categoryDAOImpl;
+
+	@Autowired
+	ReleaseDAOImpl releaseDAOImpl;
+
+	@Autowired
+	SearchQueryDAOImpl searchQueryDAOImpl;
+
+	@Autowired
+	SearchQueryPreviousResultDAOImpl searchQueryPreviousResultDAOImpl;
+
+	@Autowired
+	SearchResultDAOImpl searchResultDAOImpl;
+
+	@Test
+	public void testCategoryDAOImpl() {
+		Assert.assertNotNull(categoryDAOImpl);
+	}
+
+	@Test
+	public void testReleaseDAOImpl() {
+		Assert.assertNotNull(releaseDAOImpl);
+	}
+
+	@Test
+	public void testSearchQueryDAOImpl() {
+		Assert.assertNotNull(searchQueryDAOImpl);
+	}
+
+	@Test
+	public void testSearchQueryPreviousResultDAOImpl() {
+		Assert.assertNotNull(searchQueryPreviousResultDAOImpl);
+	}
+
+	@Test
+	public void testSearchResultDAOImpl() {
+		Assert.assertNotNull(searchResultDAOImpl);
+	}
+}
