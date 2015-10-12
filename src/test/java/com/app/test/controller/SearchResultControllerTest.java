8053deac31b18770f883da2a3a8975806f92b846
Jonathan McCann <jonathan.mccann@liferay.com>
2015-10-12 14:11:03 -0700

    Add tests to have 100% coverage

--- src/test/java/com/app/test/controller/SearchResultControllerTest.java
+++ src/test/java/com/app/test/controller/SearchResultControllerTest.java
@@ -14,12 +14,15 @@
 
 package com.app.test.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.app.exception.DatabaseConnectionException;
+import com.app.model.SearchResult;
 import com.app.test.BaseTestCase;
+import com.app.util.DatabaseUtil;
+import com.app.util.PropertiesKeys;
+import com.app.util.SearchQueryUtil;
 
+import com.app.util.SearchResultUtil;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -28,8 +31,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Date;
+
+import static org.hamcrest.Matchers.hasProperty;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * @author Jonathan McCann
@@ -44,10 +58,71 @@ public class SearchResultControllerTest extends BaseTestCase {
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
 	}
 
+	@After
+	public void tearDown() throws Exception {
+		SearchQueryUtil.deleteSearchQueries();
+
+		SearchResultUtil.deleteSearchResult(1);
+	}
+
+	@Test
+	public void testHandleError() throws Exception {
+		DatabaseUtil.setDatabaseProperties("test", "test", "test");
+
+		this.mockMvc.perform(get("/result"))
+			.andExpect(status().isFound())
+			.andExpect(view().name("redirect:error.jsp"));
+
+		String databasePassword = System.getProperty(
+			PropertiesKeys.JDBC_DEFAULT_PASSWORD);
+		String databaseURL = System.getProperty(
+			PropertiesKeys.JDBC_DEFAULT_URL);
+		String databaseUsername = System.getProperty(
+			PropertiesKeys.JDBC_DEFAULT_USERNAME);
+
+		DatabaseUtil.setDatabaseProperties(
+			databaseURL, databaseUsername, databasePassword);
+	}
+
 	@Test
 	public void testViewSearchResults() throws Exception {
 		this.mockMvc.perform(get("/result"))
 			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("searchResultMap"))
+			.andExpect(view().name("view_search_query_results"))
+			.andExpect(
+				forwardedUrl("/WEB-INF/jsp/view_search_query_results.jsp"));
+	}
+
+	@Test
+	public void testViewSearchResultsWithSearchQuery() throws Exception {
+		SearchQueryUtil.addSearchQuery("First test keywords");
+
+		this.mockMvc.perform(get("/result"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("searchResultMap"))
+			.andExpect(view().name("view_search_query_results"))
+			.andExpect(
+				forwardedUrl("/WEB-INF/jsp/view_search_query_results.jsp"));
+	}
+
+	@Test
+	public void testViewSearchResultsWithSearchQueryAndSearchResult()
+		throws Exception {
+
+		int searchQueryId = SearchQueryUtil.addSearchQuery(
+			"First test keywords");
+
+		SearchResult searchResult = new SearchResult(
+			searchQueryId, "1234", "itemTitle", 14.99, 14.99,
+			"http://www.ebay.com/itm/1234", "http://www.ebay.com/123.jpg",
+			new Date(), "Buy It Now");
+
+		SearchResultUtil.addSearchResult(searchResult);
+
+		this.mockMvc.perform(get("/result"))
+			.andExpect(status().isOk())
+			.andExpect(model().attributeExists("searchResultMap"))
 			.andExpect(view().name("view_search_query_results"))
 			.andExpect(
 				forwardedUrl("/WEB-INF/jsp/view_search_query_results.jsp"));
