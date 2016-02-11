3f449883db6578ab4e3852c944bc47e9894124f9
Jonathan McCann <jonathan.mccann@liferay.com>
2016-02-11 09:35:07 -0800

    Remove unused import

--- src/test/java/com/app/test/util/UserUtilTest.java
+++ src/test/java/com/app/test/util/UserUtilTest.java
@@ -27,8 +27,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.sql.SQLException;
-
 /**
  * @author Jonathan McCann
  */
