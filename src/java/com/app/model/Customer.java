b7c3ebaf99156c98992542a8c0e783534644d759
Jonathan McCann <jonathan.mccann@liferay.com>
2016-06-10 09:45:37 -0700

    Create a Customer model object

---
+++ src/java/com/app/model/Customer.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.model;
+
+/**
+ * @author Jonathan McCann
+ */
+public class Customer {
+
+	public Customer() {
+	}
+
+	public Customer(
+		int userId, String customerId, String subscriptionId) {
+
+		_userId = userId;
+		_customerId = customerId;
+		_subscriptionId = subscriptionId;
+	}
+
+	public int getUserId() {
+		return _userId;
+	}
+
+	public String getCustomerId() {
+		return _customerId;
+	}
+
+	public String getSubscriptionId() {
+		return _subscriptionId;
+	}
+
+	public void setUserId(int userId) {
+		_userId = userId;
+	}
+
+	public void setCustomerId(String customerId) {
+		_customerId = customerId;
+	}
+
+	public void setSubscriptionId(String subscriptionId) {
+		_subscriptionId = subscriptionId;
+	}
+
+	private int _userId;
+
+	private String _customerId;
+	private String _subscriptionId;
+
+}
