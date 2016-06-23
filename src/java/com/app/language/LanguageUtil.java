86d949189b14be720d6b651f3bd2afc0b95b0343
Jonathan McCann <jonathan.mccann@liferay.com>
2016-06-23 11:42:57 -0700

    Use ResourceBundle to keep all of the error messages in one place

---
+++ src/java/com/app/language/LanguageUtil.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.language;
+
+import java.util.ResourceBundle;
+
+/**
+ * @author Jonathan McCann
+ */
+public class LanguageUtil {
+
+	public static String getMessage(String messageKey) {
+		return _resourceBundle.getString(messageKey);
+	}
+
+	private static final ResourceBundle _resourceBundle =
+		ResourceBundle.getBundle("language");
+
+}
