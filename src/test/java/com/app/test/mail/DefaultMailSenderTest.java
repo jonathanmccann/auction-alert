9e3ec6143d0220aacf9be3a95fec9f82033bc9c9
Jonathan McCann <jonathan.mccann@liferay.com>
2016-07-19 15:39:02 -0700

    Apply similar fix to DefaultMailSenderTest

--- src/test/java/com/app/test/mail/DefaultMailSenderTest.java
+++ src/test/java/com/app/test/mail/DefaultMailSenderTest.java
@@ -19,6 +19,7 @@ import com.app.model.SearchQuery;
 import com.app.model.SearchResult;
 import com.app.test.BaseTestCase;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,6 +33,7 @@ import javax.mail.internet.InternetAddress;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.ui.velocity.VelocityEngineFactoryBean;
 
 /**
  * @author Jonathan McCann
@@ -76,6 +78,27 @@ public class DefaultMailSenderTest extends BaseTestCase {
 
 	@Test
 	public void testPopulateEmailMessage() throws Exception {
+		Field velocityEngine = _clazz.getDeclaredField("velocityEngine");
+
+		velocityEngine.setAccessible(true);
+
+		VelocityEngineFactoryBean velocityEngineFactoryBean =
+			new VelocityEngineFactoryBean();
+
+		Map<String, Object> velocityPropertiesMap = new HashMap<>();
+
+		velocityPropertiesMap.put("resource.loader", "class");
+		velocityPropertiesMap.put(
+			"class.resource.loader.class",
+			"org.apache.velocity.runtime.resource.loader." +
+				"ClasspathResourceLoader");
+
+		velocityEngineFactoryBean.setVelocityPropertiesMap(
+			velocityPropertiesMap);
+
+		velocityEngine.set(
+			_classInstance, velocityEngineFactoryBean.createVelocityEngine());
+
 		Method populateEmailMessageMethod = _clazz.getDeclaredMethod(
 			"_populateEmailMessage", Map.class, String.class, String.class,
 			String.class, Session.class);
@@ -104,13 +127,7 @@ public class DefaultMailSenderTest extends BaseTestCase {
 		Assert.assertEquals("test@test.com", message.getFrom()[0].toString());
 		Assert.assertTrue(
 			message.getSubject().contains("New Search Results - "));
-		Assert.assertEquals(
-			"Keywords: Test keywords\n\nItem: itemTitle\n" +
-				"Auction Price: $14.99\nFixed Price: $29.99\n" +
-				"URL: http://www.ebay.com/itm/1234\n\n" +
-				"<a href=\"/email_unsubscribe?emailAddress=test@test.com&" +
-				"unsubscribeToken=unsubscribeToken\">Unsubscribe</a>",
-			message.getContent());
+		Assert.assertEquals(_EMAIL_CONTENT, message.getContent());
 
 		InternetAddress[] internetAddresses = new InternetAddress[1];
 
@@ -123,5 +140,44 @@ public class DefaultMailSenderTest extends BaseTestCase {
 	private static Object _classInstance;
 	private static Class _clazz;
 	private static Session _session;
+
+	public static final String _EMAIL_CONTENT = "<html>\n" +
+		"\t<body>\n" +
+		"\t\t<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" style=\"font-size: 16px\">\n" +
+		"\t\t\t<tr>\n" +
+		"\t\t\t\t<td align=\"center\">\n" +
+		"\t\t\t\t\t<h2 style=\"color: #666f77; font-weight: 300; line-height: 1em; margin: 0 0 1em 0; text-transform: uppercase; letter-spacing: 0.125em; font-size: 1.5em; line-height: 1.5em;\">eBay Searcher</h2>\n" +
+		"\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t<table>\n" +
+		"\t\t\t\t\t\t\t\n" +
+		"\t\t\t\t\t\t\t<h3 style=\"color: #666f77; font-weight: 300; line-height: 1em; margin: 0 0 1em 0; text-transform: uppercase; letter-spacing: 0.125em; font-size: 1.25em; line-height: 1.5em; text-align: center;\">Test keywords</h3>\n" +
+		"\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t<tr>\n" +
+		"\t\t\t\t\t\t\t\t\t<td>\n" +
+		"\t\t\t\t\t\t\t\t\t\t<div style=\"display: inline-block; padding: 10px; width: 140px;\">\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t<img alt=\"http://www.ebay.com/123.jpg\" src=\"http://www.ebay.com/123.jpg\">\n" +
+		"\t\t\t\t\t\t\t\t\t\t</div>\n" +
+		"\n" +
+		"\t\t\t\t\t\t\t\t\t\t<div style=\"display: inline-block; text-align: left; vertical-align: top;\">\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t<a href=\"http://www.ebay.com/itm/1234\">itemTitle</a> <br> <br>\n" +
+		"\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tAuction Price: $14.99\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tFixed Price: $29.99\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t</div>\n" +
+		"\t\t\t\t\t\t\t\t\t</td>\n" +
+		"\t\t\t\t\t\t\t\t</tr>\n" +
+		"\t\t\t\t\t\t\t\t\t\t\t\t\t</table>\n" +
+		"\t\t\t\t\t\t\t\t\t</td>\n" +
+		"\t\t\t</tr>\n" +
+		"\t\t</table>\n" +
+		"\t\t<footer style=\"background: #f8f8f8; padding: 4em 0 6em 0; text-align: center; color: #bbb\">\n" +
+		"\t\t\t<a style=\"display: block;\" href=\"/email_unsubscribe?emailAddress=test@test.com&unsubscribeToken=unsubscribeToken\">Unsubscribe</a>\n" +
+		"\t\t\t<p style=\"text-align: center\">\n" +
+		"\t\t\t\t© eBay Searcher. All rights reserved.\n" +
+		"\t\t\t</p>\n" +
+		"\t\t</footer>\n" +
+		"\t</body>\n" +
+		"</html>";
 
 }
