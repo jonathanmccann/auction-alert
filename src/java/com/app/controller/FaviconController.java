d04a4089e401d099077d507e49e66b45739e7ba5
Jonathan McCann <jonathan.mccann@liferay.com>
2016-07-25 14:55:01 -0700

    Create a favicon controller and update the icon

---
+++ src/java/com/app/controller/FaviconController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Jonathan McCann
+ */
+@Controller
+public class FaviconController {
+
+	@RequestMapping(value = "/favicon.ico", method = RequestMethod.GET)
+	public String getFavicon() {
+		return "forward:resources/images/favicon.ico";
+	}
+
+}
