4bdad37040962a8a678e03e34b8147c158ec5963
Jonathan McCann <jonathan.mccann@liferay.com>
2017-09-29 15:18:49 -0700

    Add testing for FaviconController

---
+++ src/test/java/com/app/test/controller/FaviconControllerTest.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.test.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import com.app.test.BaseTestCase;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.powermock.modules.junit4.rule.PowerMockRule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author Jonathan McCann
+ */
+@ContextConfiguration("/test-dispatcher-servlet.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class FaviconControllerTest extends BaseTestCase {
+
+	@Rule
+	public PowerMockRule rule = new PowerMockRule();
+
+	@Before
+	public void setUp() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
+
+	@Test
+	public void testGetExceptionHandler() throws Exception {
+		this.mockMvc.perform(get("/favicon.ico"))
+			.andExpect(forwardedUrl("resources/images/favicon.ico"))
+			.andExpect(view().name("forward:resources/images/favicon.ico"));
+	}
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext wac;
+
+}
