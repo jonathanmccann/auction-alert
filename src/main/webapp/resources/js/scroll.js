9e57cc7ca73e4b79ff76a1757e58d50608cbcaaf
Jonathan McCann <jonathan.mccann@liferay.com>
2017-10-09 11:05:28 -0700

    Update scroll since the current one does nothing

--- src/main/webapp/resources/js/scroll.js
+++ src/main/webapp/resources/js/scroll.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#learn-more").click(function(){
 		$('html, body').animate({
-			scrollTop: 500-$( $(this).attr('href') ).offset().top
+			scrollTop: $($(this).attr('href')).offset().top - 32
 		}, 500);
 		return false;
 	});
