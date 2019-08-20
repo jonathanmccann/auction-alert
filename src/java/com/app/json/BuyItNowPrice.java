9e81f97c96a6b1a07d39d4d1ebcea622701d36d0
Jonathan McCann <jonathan.mccann@liferay.com>
2019-08-20 12:13:05 -0700

    Add new objects to be used with Gson deserialization

---
+++ src/java/com/app/json/BuyItNowPrice.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.json; 
+
+import com.google.gson.annotations.SerializedName;
+
+public class BuyItNowPrice {
+
+	@SerializedName("@currencyId")
+	private String currencyId;
+
+	@SerializedName("__value__")
+	private double value;
+
+	public String getCurrencyId() {
+		return currencyId;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+}
