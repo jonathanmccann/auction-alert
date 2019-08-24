106f18a5f4f86376f1d950048a04d9c1b54464e9
Jonathan McCann <jonathan.mccann@liferay.com>
2019-08-23 19:11:47 -0700

    Provide a fall back in case the exchange rate API is down

--- src/java/com/app/util/ExchangeRateUtil.java
+++ src/java/com/app/util/ExchangeRateUtil.java
@@ -15,6 +15,8 @@
 package com.app.util;
 
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -43,28 +45,70 @@ public class ExchangeRateUtil {
 	}
 
 	public static void updateExchangeRates() throws Exception {
-		Gson gson = new Gson();
+		try {
+			Gson gson = new Gson();
 
-		for (String fromCurrencyId : _CURRENCY_IDS) {
-			for (String toCurrencyId : _CURRENCY_IDS) {
-				if (fromCurrencyId.equals(toCurrencyId)) {
-					continue;
-				}
+			for (String fromCurrencyId : _CURRENCY_IDS) {
+				for (String toCurrencyId : _CURRENCY_IDS) {
+					if (fromCurrencyId.equals(toCurrencyId)) {
+						continue;
+					}
 
-				URL exchangeRateUrl = new URL(
-					_EXCHANGE_RATE_URL + fromCurrencyId + "_" + toCurrencyId);
+					URL exchangeRateUrl = new URL(
+						_EXCHANGE_RATE_URL + fromCurrencyId + "_" + toCurrencyId);
 
-				URLConnection urlConnection = exchangeRateUrl.openConnection();
+					URLConnection urlConnection = exchangeRateUrl.openConnection();
 
-				try (InputStreamReader inputStreamReader = new InputStreamReader(
+					try (InputStreamReader inputStreamReader = new InputStreamReader(
 						urlConnection.getInputStream());
-					BufferedReader bufferedReader = new BufferedReader(
-						inputStreamReader)) {
+						 BufferedReader bufferedReader = new BufferedReader(
+							 inputStreamReader)) {
 
-					_exchangeRates.putAll(
-						gson.fromJson(bufferedReader, Map.class));
+						_exchangeRates.putAll(
+							gson.fromJson(bufferedReader, Map.class));
+					}
 				}
 			}
+		}
+		catch (Exception e) {
+			_log.error(
+				"Unable to fetch exchange rates from API", e.getMessage(), e);
+
+			_exchangeRates.put("AUD_CAD", 0.95);
+			_exchangeRates.put("AUD_CHF", 0.71);
+			_exchangeRates.put("AUD_EUR", 0.63);
+			_exchangeRates.put("AUD_GBP", 0.55);
+			_exchangeRates.put("AUD_USD", 0.71);
+
+			_exchangeRates.put("CAD_AUD", 1.05);
+			_exchangeRates.put("CAD_CHF", 0.75);
+			_exchangeRates.put("CAD_EUR", 0.67);
+			_exchangeRates.put("CAD_GBP", 0.58);
+			_exchangeRates.put("CAD_USD", 0.75);
+
+			_exchangeRates.put("CHF_AUD", 1.40);
+			_exchangeRates.put("CHF_CAD", 1.33);
+			_exchangeRates.put("CHF_EUR", 0.89);
+			_exchangeRates.put("CHF_GBP", 0.77);
+			_exchangeRates.put("CHF_USD", 1.00);
+
+			_exchangeRates.put("EUR_AUD", 1.58);
+			_exchangeRates.put("EUR_CAD", 1.50);
+			_exchangeRates.put("EUR_CHF", 1.13);
+			_exchangeRates.put("EUR_GBP", 0.86);
+			_exchangeRates.put("EUR_USD", 1.13);
+
+			_exchangeRates.put("GBP_AUD", 1.83);
+			_exchangeRates.put("GBP_CAD", 1.74);
+			_exchangeRates.put("GBP_CHF", 1.30);
+			_exchangeRates.put("GBP_EUR", 1.16);
+			_exchangeRates.put("GBP_USD", 1.31);
+
+			_exchangeRates.put("USD_AUD", 1.40);
+			_exchangeRates.put("USD_CAD", 1.33);
+			_exchangeRates.put("USD_CHF", 1.00);
+			_exchangeRates.put("USD_EUR", 0.89);
+			_exchangeRates.put("USD_GBP", 0.77);
 		}
 	}
 
@@ -75,6 +119,9 @@ public class ExchangeRateUtil {
 	private static final String _EXCHANGE_RATE_URL =
 		"https://free.currencyconverterapi.com/api/v6/convert?compact=ultra&apiKey=" +
 			PropertiesValues.CURRENCY_CONVERTER_API_KEY + "&q=";
+
+	private static final Logger _log = LoggerFactory.getLogger(
+		ExchangeRateUtil.class);
 
 	private static final Map<String, Double> _exchangeRates =
 		new HashMap<>();
