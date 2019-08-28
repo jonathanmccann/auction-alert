f2b7b8fc27bc2be6a3330755099b6a41c6e144a8
Jonathan McCann <jonathan.mccann@liferay.com>
2019-08-28 14:12:17 -0700

    Remove unnecessary test and context from MailSenderFactoryTest

--- src/test/java/com/app/test/mail/MailSenderFactoryTest.java
+++ src/test/java/com/app/test/mail/MailSenderFactoryTest.java
@@ -18,13 +18,11 @@ import com.app.mail.DefaultMailSender;
 import com.app.mail.MailSender;
 import com.app.mail.MailSenderFactory;
 import com.app.mail.SendGridMailSender;
-import com.app.test.BaseTestCase;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -33,17 +31,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * @author Jonathan McCann
  */
 @ContextConfiguration("/test-active-dispatcher-servlet.xml")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-public class MailSenderFactoryTest extends BaseTestCase {
-
-	@Test
-	public void testGetInstance() throws Exception {
-		MailSender mailSender = MailSenderFactory.getInstance();
-
-		Assert.assertTrue(mailSender instanceof SendGridMailSender);
-	}
+public class MailSenderFactoryTest {
 
 	@Test
 	public void testSetMailSender() throws Exception {
