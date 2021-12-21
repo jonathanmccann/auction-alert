fdac862b958ca962a2914d502d963e5c2ce35767
Jonathan McCann <jonathan.mccann@liferay.com>
2021-12-21 09:19:56 -0800

    Add new utility for OAuth requests

---
+++ src/java/com/app/exception/OAuthTokenException.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.exception;
+
+/**
+ * @author Jonathan McCann
+ */
+public class OAuthTokenException extends Exception {
+
+	public OAuthTokenException() {
+	}
+
+}
---
+++ src/java/com/app/util/OAuthTokenUtil.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2014-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.app.util;
+
+import com.app.exception.OAuthTokenException;
+
+import com.ebay.api.client.auth.oauth2.OAuth2Api;
+import com.ebay.api.client.auth.oauth2.model.AccessToken;
+import com.ebay.api.client.auth.oauth2.model.Environment;
+import com.ebay.api.client.auth.oauth2.model.OAuthResponse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @author Jonathan McCann
+ */
+public class OAuthTokenUtil {
+
+	public static String getAccessToken() throws Exception {
+		OAuth2Api oauth2Api = new OAuth2Api();
+
+		OAuthResponse oauth2Response = oauth2Api.getApplicationToken(
+			Environment.PRODUCTION, SCOPE_LIST_PRODUCTION);
+
+		Optional<AccessToken> applicationToken =
+			oauth2Response.getAccessToken();
+
+		if (!applicationToken.isPresent()) {
+			throw new OAuthTokenException();
+		}
+
+		AccessToken accessToken = applicationToken.get();
+
+		return accessToken.getToken();
+	}
+
+	public static String executeRequest(String url) throws Exception {
+		HttpClient httpClient = HttpClients.createDefault();
+
+		HttpGet httpGet = new HttpGet(url);
+
+		httpGet.addHeader("Authorization", "Bearer " + getAccessToken());
+
+		HttpResponse response = httpClient.execute(httpGet);
+
+		return EntityUtils.toString(response.getEntity());
+	}
+
+	private static final List<String> SCOPE_LIST_PRODUCTION =
+		new ArrayList<String>() {{
+			add("https://api.ebay.com/oauth/api_scope");
+		}};
+
+}
